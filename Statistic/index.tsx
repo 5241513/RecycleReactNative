0fa6605cdddca31d9a77ec0e588397ef9d643585
Othern <willy910928@gmail.com>
2024-08-25 19:24:09 +0800

    revise line chart

--- Statistic/index.tsx
+++ Statistic/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
 import BarChartComponent from './BarChart';
 import LineChartComponent from './LineChart';
 import PieChartComponent from './PieChart';
 
 const MainPage = () => {
+    const [focusCategory,setFocusCategory] = useState('')
+    
     return (
         <ScrollView contentContainerStyle={styles.content}>
             <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 16,marginBottom:10 }}>累積統計數據</Text>
             <PieChartComponent />
 
+            <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 40,marginBottom:10 }}>{focusCategory}當周統計數據</Text>
+            <LineChartComponent focusCategory={focusCategory} setFocusCategory={setFocusCategory}/>
+
             <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 40,marginBottom:10 }}>今日統計數據</Text>
             <BarChartComponent />
 
-            <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 40,marginBottom:10 }}>當周統計數據</Text>
-            <LineChartComponent />
+            
         </ScrollView>
     );
 };
